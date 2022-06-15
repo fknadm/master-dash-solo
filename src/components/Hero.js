4d980fd09ec16b6d3aa6e063b7e7e90c3f10a316
adam H <aeris.contactme@gmail.com>
2022-06-16 02:44:11 +0400

    fresh

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css"
+const Hero = () => (
+  <div style={{fontSize:"18px"}} className="text-center hero my-5">
+    <h1 className="mb-4">Master Dashboard</h1>
+
+    <p style={{fontSize:"18px"}} className="lead">
+      This is a Dashboard for XERO PRIME / FOUNDING BROKERS / IMCS usage to track inter-dept. ticketing, requests and overall client performance.
+    </p>
+  </div>
+);
+
+export default Hero;
